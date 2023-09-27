cdafbe6987795f7313ec177f7f36c227caccff66
namithachevari <namithachevari@gmail.com>
2023-09-27 00:39:33 -0400

    updated the login page

--- wehvs-frontend/src/components/UserLogin.js
+++ wehvs-frontend/src/components/UserLogin.js
@@ -3,7 +3,7 @@ import { Form } from "react-bootstrap";
 import withRouter from "./Router/withRouter";
 import { Link } from "react-router-dom";
 
-class UserCreate extends Component {
+class UserLogin extends Component {
   constructor() {
     super();
     this.state = {
@@ -204,51 +204,6 @@ class UserCreate extends Component {
     const { errors, successMessage, errorMessage } = this.state;
     return (
       <div>
-        
-        {/* HEADER */}
-       <div className="header-area header-transparrent">
-           <div className="headder-top header-sticky">
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-3 col-md-2">
-                            <div className="logo">
-                                <a href="index.html"><img src="/images/logo/wehvs-logo-1.jpeg" width="220px" alt=""/></a>
-                            </div>  
-                        </div>
-                        <div className="col-lg-9 col-md-9">
-                            <div className="menu-wrapper">
-                                <div className="main-menu">
-                                    <nav className="d-none d-lg-block">
-                                        <ul id="navigation">
-                                            <li><a href="index.html">Home</a></li>
-                                            <li><a href="job_listing.html">Find a Jobs </a></li>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="#">Page</a>
-                                                <ul className="submenu">
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="single-blog.html">Blog Details</a></li>
-                                                    <li><a href="elements.html">Elements</a></li>
-                                                    <li><a href="job_details.html">job Details</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="contact.html">Contact</a></li>
-                                        </ul>
-                                    </nav>
-                                </div>          
-                                <div className="header-btn d-none f-right d-lg-block">
-                                    <a href="#" className="btn head-btn1">Register</a>
-                                    <a href="#" className="btn head-btn2">Login</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <div className="mobile_menu d-block d-lg-none"></div>
-                        </div>
-                    </div>
-                </div>
-           </div>
-       </div>
-
       {/* CONTENT */}
       <div className="row">
                     <div className="col-12">
@@ -379,4 +334,4 @@ class UserCreate extends Component {
   }
 }
 
-export default withRouter(UserCreate);
+export default withRouter(UserLogin);
