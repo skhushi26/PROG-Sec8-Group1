d688bbe6a08066ef4d2fa2eb874b9df5f771bba7
namithachevari <namithachevari@gmail.com>
2023-09-27 01:04:30 -0400

    updated login

--- wehvs-frontend/src/components/Router/Routing.js
+++ wehvs-frontend/src/components/Router/Routing.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import UserCreate from "../UserCreate";
 import UserLogin from "../UserLogin";
 import Navbar from "../Navigation";
+import FooterMenu from "../Footer";
 import NotFound from "../NotFound";
 
 export default function Routing() {
@@ -14,6 +15,7 @@ export default function Routing() {
           <Route path="/login" element={<UserLogin />} />
           <Route path="*" element={<NotFound />} />
         </Route>
+        {/* <Route path="/" element={<FooterMenu />} /> */}
       </Routes>
     </>
   );
