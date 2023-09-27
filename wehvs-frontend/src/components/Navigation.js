4b38ff18af52b70cdbfc23a1d51bd3b701c80a6c
namithachevari <namithachevari@gmail.com>
2023-09-27 16:14:49 -0400

    updated

--- wehvs-frontend/src/components/Navigation.js
+++ wehvs-frontend/src/components/Navigation.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                                 <div className="main-menu">
                                     <nav className="d-none d-lg-block">
                                         <ul id="navigation">
-                                            <li><a href="index.html">Home</a></li>
+                                            <li><a href="index.html">Dashboard</a></li>
                                             <li><a href="job_listing.html">Find a Job </a></li>
                                             <li><a href="about.html">About</a></li>
                                             {/* <li><a href="#">Page</a>
