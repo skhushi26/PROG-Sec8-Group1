8b91aeb23fe386f49ebd9505dba8f8578b22c55c
namithachevari <namithachevari@gmail.com>
2023-09-27 16:08:16 -0400

    updated the header menu

--- wehvs-frontend/src/components/Navigation.js
+++ wehvs-frontend/src/components/Navigation.js
@@ -21,14 +21,14 @@ const Navbar = () => {
                                             <li><a href="index.html">Home</a></li>
                                             <li><a href="job_listing.html">Find a Jobs </a></li>
                                             <li><a href="about.html">About</a></li>
-                                            <li><a href="#">Page</a>
+                                            {/* <li><a href="#">Page</a>
                                                 <ul className="submenu">
                                                     <li><a href="blog.html">Blog</a></li>
                                                     <li><a href="single-blog.html">Blog Details</a></li>
                                                     <li><a href="elements.html">Elements</a></li>
                                                     <li><a href="job_details.html">job Details</a></li>
                                                 </ul>
-                                            </li>
+                                            </li> */}
                                             <li><a href="contact.html">Contact</a></li>
                                         </ul>
                                     </nav>
